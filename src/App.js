a4e537658bb3784dd18b46d94aa96c4bf4a84114
Rasha Zayed <zayed.rasha@outlook.com>
2020-10-03 22:57:29 -0700

    Person card with birthday button to calculate the age

--- src/App.js
+++ src/App.js
@@ -6,24 +6,20 @@ function App() {
   return (
     <div className="App">
       <div className="card">
-        <PersonCard lastName="Zayed, " firstName='Rasha'/>
-        <PersonCard age='Age: 30'/>
-        <PersonCard hairColor = "Hair Color: Brown"/>
+        <PersonCard lastName=" Zayed"firstName=' Rasha' age={30} hairColor = "Hair Color: Brown"/>
+        
       </div>
       <div className="card">
-        <PersonCard lastName='Smith, ' firstName="John"/>
-        <PersonCard age='Age :88'/>
-        <PersonCard hairColor="Hair Color: Brown"/>
+        <PersonCard lastName=' Smith' firstName=" John" age={88} hairColor="Hair Color: Brown"/>
+       
       </div>
       <div className="card">
-        <PersonCard lastName='Fillmore, ' firstName='Millard'/>
-        <PersonCard age="Age : 50"/>
-        <PersonCard hairColor="Hair Color: Black"/>
+        <PersonCard lastName=' Fillmore' firstName=' Millard' age={50} hairColor="Hair Color: Black"/>
+        
       </div>
       <div className="card">
-        <PersonCard lastName='Smith, ' firstName='Maria'/>
-        <PersonCard age="Age : 62"/>
-        <PersonCard hairColor="Hair Color: Brown"/>
+        <PersonCard lastName=' Smith' firstName=' Maria' age={62} hairColor="Hair Color: Brown"/>
+        
       </div>
       </div>
   );
--- src/MyComponents.js
+++ src/MyComponents.js
@@ -1,23 +1,38 @@
 import React, {Component} from 'react'
 
 class PersonCard extends Component {
+    constructor(props){
+        super(props);
+        this.state= {
+           age: this.props.age
+        }
+
+    }
+    ageInc =()=> {
+        this.setState({ age: this.state.age + 1 });
+    
+     }
     render () {
         return (
         <div>
            <h1>
-                {this.props.lastName}
-               {this.props.firstName}
+                {this.props.lastName},
+                {this.props.firstName}
                
            </h1>
            <div>
-               {this.props.age}
+               Age: {this.state.age}
            </div>
            <div>
                {this.props.hairColor}
            </div>
+           {this.props.children}
+           <button onClick={this.ageInc}>Birthday Button for {this.props.firstName}{this.props.lastName}</button>
         </div>
 
         )
-    }
+  
+  }
 }
+
 export default PersonCard ;
